15db94e10b930fa0b2b3fb3e294cbeee73de93fb
Jorge Filho <jorgeflh@gmail.com>
2018-05-21 11:12:44 -0300

    New context for Entity Framework Code First

---
+++ MedicalScheduling/Data/MedicalSchedulingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MedicalScheduling.Models;
+
+    public class MedicalSchedulingContext : DbContext
+    {
+        public MedicalSchedulingContext (DbContextOptions<MedicalSchedulingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MedicalScheduling.Models.Users> Users { get; set; }
+    }
--- MedicalScheduling/Startup.cs
+++ MedicalScheduling/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedicalScheduling
 {
@@ -23,6 +24,9 @@ namespace MedicalScheduling
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MedicalSchedulingContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MedicalSchedulingContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
